13639a9fa10bedd98d24059e842da0982e67586f
yousefhgag9 <160474274+yousefhgag9@users.noreply.github.com>
2025-07-18 13:50:50 +0300

    Add .next/static/chunks/src_app_analytics_page_tsx_78462eda._.js 1752835874555

---
+++ .next/static/chunks/src_app_analytics_page_tsx_78462eda._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_18c1b18f._.js",
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_e01b20c9._.js",
+  "static/chunks/node_modules_b82c1e3f._.js"
+],
+    source: "dynamic"
+});
